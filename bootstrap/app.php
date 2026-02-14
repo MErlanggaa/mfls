7e05b4ef6d7af13ac52c66ba9a3cf5ed5365204d
MErlanggaa <muhammaderlanggaputraw@gmail.com>
2026-02-15 01:20:43 +0700

    update cors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Foundation\Http\Middleware\HandleCors;
+use App\Http\Middleware\CorsMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(HandleCors::class);
+        $middleware->append(CorsMiddleware::class);
 
         $middleware->alias([
             'survey.check' => \App\Http\Middleware\EnsureSurveyIsFilled::class,
