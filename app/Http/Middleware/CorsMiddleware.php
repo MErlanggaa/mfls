24851ef3884eeac3602ec621d88ae41d17d843a4
MErlanggaa <muhammaderlanggaputraw@gmail.com>
2026-02-15 02:00:08 +0700

    update cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -9,16 +9,20 @@ class CorsMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
+        // Tangani Preflight Request (OPTIONS)
         if ($request->isMethod('OPTIONS')) {
-            return response('', 200)
-                ->header('Access-Control-Allow-Origin', 'http://localhost:5173')
+            return response('', 204) // 204 No Content lebih standar untuk OPTIONS
+                ->header('Access-Control-Allow-Origin', '*') // Ganti * dengan domain React kamu nanti
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
         }
 
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:5173')
+        $response = $next($request);
+
+        // Tambahkan header ke response asli
+        return $response
+            ->header('Access-Control-Allow-Origin', '*') 
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\CorsMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,8 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(CorsMiddleware::class);
-
+        $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
+        
+       
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'survey.check' => \App\Http\Middleware\EnsureSurveyIsFilled::class,
         ]);
