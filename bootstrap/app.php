b3836a25003a1bf01e78c57907e53540dccba51a
MErlanggaa <muhammaderlanggaputraw@gmail.com>
2026-02-17 00:20:54 +0700

    update cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,10 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-
-        // ✅ Pakai CORS bawaan Laravel 11
-        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
-
+        
         // Proxy trust
         $middleware->trustProxies(at: '*');
 
