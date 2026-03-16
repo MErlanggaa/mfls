f08a6e1d29cd1fc2294f72a3d7de9da4e1be710f
MErlanggaa <muhammaderlanggaputraw@gmail.com>
2026-03-16 13:45:16 +0700

    update page view

---
+++ database/migrations/2026_03_16_064240_increase_column_lengths_in_page_views_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('page_views', function (Blueprint $table) {
+            $table->text('url')->change();
+            $table->text('user_agent')->nullable()->change();
+            $table->text('referer')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('page_views', function (Blueprint $table) {
+            $table->string('url', 255)->change();
+            $table->string('user_agent', 255)->nullable()->change();
+            $table->string('referer', 255)->nullable()->change();
+        });
+    }
+};
