0e25cda6fdd38f90907de729ba63d78b823b0b50
MErlanggaa <muhammaderlanggaputraw@gmail.com>
2026-03-22 00:38:46 +0700

    update

--- resources/views/pendaftar/berkas.blade.php
+++ resources/views/pendaftar/berkas.blade.php
@@ -474,7 +474,7 @@
                     <div class="w-8 h-8 bg-purple-500/10 rounded-lg flex items-center justify-center">
                         <svg class="w-5 h-5 text-purple-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"/></svg>
                     </div>
-                    Link Video - Membuat Video Motivasi
+                    Link Video - Peserta Wajib Membuat Video Motivasi
                 </h3>
 
                 <form action="{{ route('pendaftar.berkas.store') }}" method="POST">
