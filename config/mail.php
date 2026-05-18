cd9390cac5e3c38307092d959a95843c5751992d
MErlanggaa <muhammaderlanggaputraw@gmail.com>
2026-05-18 23:43:05 +0700

    updateee

--- config/mail.php
+++ config/mail.php
@@ -43,7 +43,7 @@ return [
             'port' => env('MAIL_PORT', 2525),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
-            'password' => env('HOSTINGER_PW'),
+            'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
             'stream' => [
